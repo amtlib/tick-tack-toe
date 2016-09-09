f16fcb25977cc33987eb151023af3a87cb2772d6
Grzegorz Pach <maszeczuszets@protonmail.com>
2016-09-09 18:49:10 +0200

    created function which generates array of divs with proper IDs, classes and sizes. Basic styling of cells

--- js/script.js
+++ js/script.js
@@ -1,3 +1,19 @@
-$(document).ready(function(){
+var cells = [];
+var size = 3; //3x3 board
 
+$(document).ready(function () {
+    generate(size);
 });
+
+function generate(size) {
+    cells = [];
+    var current_div;
+    for (var i = 0; i < size * size; i++) {
+        current_div = $('<div><div>');
+        current_div.attr('id', 'cell_' + i);
+        current_div.attr('class', 'cell');
+        current_div.width($(window).width() / size - 20);
+        current_div.height($(window).width() / size - 20);
+        cells.push(current_div);
+    }
+}
